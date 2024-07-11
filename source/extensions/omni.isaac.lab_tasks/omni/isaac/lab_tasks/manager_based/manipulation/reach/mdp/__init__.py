24f2487de7236b181fc5f44e84cbfd57bcc8c30a
Mike Groom <Mike.Groom@jacobs.com>
2024-07-11 17:35:00 +0000

    cirriculum for hsr reach task

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/reach/mdp/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/reach/mdp/__init__.py
@@ -20,3 +20,5 @@ from omni.isaac.lab.envs.mdp import *  # noqa: F401, F403
 
 from .rewards import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
+from .curriculums import *  # noqa: F401, F403
+
